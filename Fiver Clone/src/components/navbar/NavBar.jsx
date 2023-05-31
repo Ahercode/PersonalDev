781ee9b68716917c0244356aa848c8568b18f633
Ahercode <philipkelly407@gmail.com>
2023-05-31 03:31:50 +0000

    fix 3

--- Fiver Clone/src/components/navbar/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./NavBar.scss"
-function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
-}
-
-export default NavBar
--- Fiver Clone/src/pages/addGig/AddGid.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./AddGid.scss"
-function AddGid() {
-  return (
-    <div>AddGid</div>
-  )
-}
-
-export default AddGid
